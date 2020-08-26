351a9ea821ffe106439b8056cc2aa0c35a591ecf
Bogdan Chadkin <trysound@yandex.ru>
2020-08-26 15:31:19 +0300

    Replace winston with custom logger
    Ref https://packagephobia.com/result?p=winston
    
    I just found winston package is quote big for such simple task as
    colorized loggin. At least in this project all winston power was not used.
    
    In this diff I added very basic loggin solution with for levels "error",
    "warn", "info" and "debug".

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,28 @@
-import { createLogger, transports, format } from 'winston';
+import * as colors from 'ansi-colors';
 
-export const logger = createLogger({
-  transports: [new transports.Console()],
-  format: format.combine(format.colorize(), format.simple()),
-});
+type Level = 'error' | 'warn' | 'info' | 'debug';
+
+const currentLevel: Level = 'info';
+
+const levels: Level[] = ['error', 'warn', 'info', 'debug'];
+
+const log = (level: Level, color: any, args: any[]) => {
+  if (levels.indexOf(level) <= levels.indexOf(currentLevel)) {
+    console.log(`${color(level)}:`, ...args);
+  }
+};
+
+export const logger = {
+  error: (...args: any[]) => {
+    log('error', colors.red, args);
+  },
+  warn: (...args: any[]) => {
+    log('warn', colors.yellow, args);
+  },
+  info: (...args: any[]) => {
+    log('info', colors.green, args);
+  },
+  debug: (...args: any[]) => {
+    log('debug', colors.blue, args);
+  },
+};
