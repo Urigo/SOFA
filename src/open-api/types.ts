1826ced2afba8b0b0429137abcbdb8e31761ba81
izumin5210 <m@izum.in>
2022-09-08 15:39:07 +0900

    Stop using AST Node to retrieve Enum values in OpenAPI

--- src/open-api/types.ts
+++ src/open-api/types.ts
@@ -79,7 +79,7 @@ export function resolveFieldType(
   if (isEnumType(type)) {
     return {
       type: 'string',
-      enum: type.astNode?.values?.map((value) => value.name.value),
+      enum: type.getValues().map((value) => value.name),
     };
   }
 
