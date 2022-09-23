e159674430f85f9b0a9b06a858e467717ef5330c
TuvalSimha <tuval.simha@gmail.com>
2022-09-23 08:29:09 +0300

    Fix animation

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import '@theguild/components/style.css';
+import '../components/animation-container.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
