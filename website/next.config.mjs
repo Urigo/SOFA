899058f4f511b0b40cba85366edcc99f56b729c5
TuvalSimha <tuval.simha@gmail.com>
2022-09-23 08:39:12 +0300

    add redirects

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,3 +1,10 @@
 import { withGuildDocs } from '@theguild/components/next.config';
 
-export default withGuildDocs();
+export default withGuildDocs({
+  redirects: () =>
+    Object.entries({}).map(([from, to]) => ({
+      source: from,
+      destination: to,
+      permanent: true,
+    })),
+});
