b1ccc999c8ccebcb17f60495bcd92eb2f87d2e6b
Dotan Simha <dotansimha@gmail.com>
2022-10-03 11:10:12 +0700

    update website to latest setup (#1188)

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,7 +2,7 @@ import { withGuildDocs } from '@theguild/components/next.config';
 
 export default withGuildDocs({
   images: {
-    unoptimized: true, // doesn't work with `next export`
+    unoptimized: true,
   },
   redirects: () =>
     Object.entries({}).map(([from, to]) => ({
