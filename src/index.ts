a7404a2e2681bf0d36253d9ee8f9eff1f5faea41
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-07-25 19:41:08 +0300

    v0.18.8

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createRouter } from './router.js';
 import type { SofaConfig } from './sofa.js';
 import { createSofa } from './sofa.js';
 
-export { OpenAPI } from './open-api';
+export { OpenAPI } from './open-api/index.js';
 
 export function useSofa(config: SofaConfig) {
   return createRouter(
